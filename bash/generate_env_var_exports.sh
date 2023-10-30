f1d60428aeae41f69b31b026c22ac7c4348ce3f0
Tapanchugh <149379888+Tapanchugh@users.noreply.github.com>
2023-10-30 17:12:22 +0530

    Update generate_env_var_exports.sh

--- bash/generate_env_var_exports.sh
+++ bash/generate_env_var_exports.sh
@@ -7,8 +7,8 @@ TERRAFORM_DIR="${CURRENT_DIR}/../terraform"
 
 
 # Authentication
-echo "export TF_VAR_AWS_SECRET_KEY=pMglRC4geRnNNZpC9SYqlLvse4BHKKzh7Vi0+H3x"
-echo "export TF_VAR_AWS_ACCESS_KEY=AKIARBUWI3SOP4KEKN4Q"
+echo "export TF_VAR_AWS_SECRET_KEY="
+echo "export TF_VAR_AWS_ACCESS_KEY="
 
 # Resource Location
 echo "export TF_VAR_AWS_REGION=us-east-2"
